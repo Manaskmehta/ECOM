75e186e411727f43dd8f2c00b2711c1df0a9400c
Manaskmehta <145827159+Manaskmehta@users.noreply.github.com>
2024-04-17 23:27:28 +0530

    UPLOAD
    1

---
+++ assets/ecom.js
@@ -0,0 +1,60 @@
+const product = [
+    {
+        id: 0,
+        title: 'LONDON CLAASIC',
+        image: 'assets/images/shoe2.jpeg',
+        price: 11999,
+
+    },
+    {
+        id: 1,
+        title: 'Nike Air FORCE 1',
+        image: 'assets/images/shoe4.jpeg',
+        price: 15999,
+
+    },
+    {
+        id: 2,
+        title: 'SHOW STOPPER',
+        image: 'assets/images/shoe6.jpeg',
+        price: 29999,
+
+    },
+    {
+        id: 3,
+        title: 'DUNK LOWS',
+        image: 'assets/images/SHOE8.png',
+        price: 26999,
+
+    },
+    {
+        id: 4,
+        title: 'Air 1 Supreme Edition',
+        image: 'assets/images/shoe1.webp',
+        price: 25999,
+
+    },
+    {
+        id: 5,
+        title: 'Runner 350',
+        image: 'assets/images/shoe5.webp',
+        price: '20999',
+
+    },
+    {
+        id: 6,
+        title: 'Nike Air FORCE 1 CLASSIC',
+        image: 'assets/images/shoe3.jpeg',
+        price: '17999',
+
+    },
+    {
+        id: 7,
+        title: 'Nike DUNK LOW',
+        image: 'assets/images/shoes7.webp',
+        price: '27999',
+
+    },
+
+]
+let sum = 0;
